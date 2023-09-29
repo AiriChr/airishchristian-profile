c10e6bb5cf55116ba992cc60f5428c33205a168e
AC <airishchristian@gmail.com>
2023-09-29 23:08:57 +0800

    Added files

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+const Home = () => {
+  return(
+    <main>
+      <div>
+        <h1>
+          Hello World!
+        </h1>
+      </div>
+    </main>
+  )
+}
+
+export default Home
