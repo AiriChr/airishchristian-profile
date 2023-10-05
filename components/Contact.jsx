e5cef33f7b552ff1044cd7c88e26027ad7155300
AiriChr <44491497+AiriChr@users.noreply.github.com>
2023-10-05 22:34:36 +0800

    Added files
    Responsive page

---
+++ components/Contact.jsx
@@ -0,0 +1,93 @@
+import Link from 'next/link'
+import React from 'react'
+import { AiOutlineMail } from 'react-icons/ai'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
+import { HiOutlineChevronDoubleUp } from 'react-icons/hi'
+
+const Contact = () => {
+  return (
+    <div id='contact' className='w-full lg:h-screen'>
+        <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
+            <p className='text-xl tracking-widest uppercase text-cyan-200'>Contact</p>
+            <h2 className='py-4'>Get In Touch</h2>
+            <div className='grid lg:grid-cols-5 gap-8'>
+                {/*left */}
+                <div className='col-span-3 lg:col-span-2 w-full h-full shadow-md shadow-gray-400 rounded-xl p-4'>
+                    <div className='lg:p-4 h-full'>
+                        <div>
+                            <img className='rounded-xl hover:scale-105 ease-in duration-300'src="https://images.unsplash.com/photo-1587560699334-bea93391dcef?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2940&q=80" alt="" />
+                        </div>
+                        <div>
+                            <h2 className='py-2'>AC Tabay</h2>
+                            <p>NextJS Full-Stack Developer</p>
+                            <p className='py-4'>I am available for freelance or full-time positions. Contact me and let's talk.</p>
+                        </div>
+                        <div>
+                            <p className='uppercase pt-8'>Connect With Me</p>
+                            <div className='flex items-center justify-between py-4'>
+                                <div className='rounded-full shadow-md shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-10'>
+                                    <FaLinkedinIn />
+                                </div>
+                                <div className='rounded-full shadow-md shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-10'>
+                                    <FaGithub />
+                                </div>
+                                <div className='rounded-full shadow-md shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-10'>
+                                    <AiOutlineMail />
+                                </div>
+                                <div className='rounded-full shadow-md shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-10'>
+                                    <BsFillPersonLinesFill />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/*right */}
+
+                <div className='col-span-3 w-full h-auto shadow-md shadow-gray-400 rounded-xl lg:p-4'>
+                    <div className='p-4'>
+                        <form>
+                            <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+                                <div className='flex flex-col'>
+                                    <label className='uppercase text-sm py-2'>Name</label>
+                                    <input className='border-2 rounded-lg p-3 flex border-gray-300'type="text" />
+                                </div>
+                                <div className='flex flex-col'>
+                                    <label className='uppercase text-sm py-2'>Phone Number</label>
+                                    <input className='border-2 rounded-lg p-3 flex border-gray-300'type="text" />
+                                </div>
+                            </div>
+                            <div className='flex flex-col py-2'>
+                                    <label className='uppercase text-sm py-2'>Email</label>
+                                    <input className='border-2 rounded-lg p-3 flex border-gray-300'type="text" />
+                            </div>
+                            <div className='flex flex-col py-2'>
+                                    <label className='uppercase text-sm py-2'>Subject</label>
+                                    <input className='border-2 rounded-lg p-3 flex border-gray-300'type="text" />
+                            </div>
+                            <div className='flex flex-col py-2'>
+                                    <label className='uppercase text-sm py-2'>Message</label>
+                                    <textarea className='border-2 rounded-lg p-3 border-gray-300' rows='10'></textarea>
+                            </div>
+                            <div >
+                                <button className='w-full p-4 text-gray-100 mt-4 shadow-sm shadow-gray-400'>Send Message</button>
+                            </div>
+                            
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+            <div className='flex justify-center py-12'>
+                <Link href='/'>
+                <div className='rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-10'>
+                        <HiOutlineChevronDoubleUp className='text-[#5651e5]'size={30}/>
+                    </div>
+                </Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Contact
---
+++ components/Projects.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ProjectItem from './ProjectItem';
+
+
+const Projects = () => {
+  return (
+    <div id='projects'>
+        <div className='max-w-[1240px] mx-auto px-2 py-16'>
+            <p className='text-xl tracking-widest uppercase text-cyan-200'>Projects</p>
+            <h2 className='py-4'>What I have Built</h2>
+            <ProjectItem /> {/* responsive */}
+        </div>
+    </div>
+  )
+}
+
+export default Projects
